3efd90540bb5ca10dfd248fe025907074e80ba03
fspadea <fs279@pvsd.org>
2016-10-18 14:17:43 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/* public int compareTo(object myObj)
+	* takes a String and alphabetically compares it to another String to see which String would come first in an alphabetically ordered list.
+	* It returns a positive integer if the String making the call precedes the String in the paranthesis
+	* a negative is returned if the String comes after the one in the paranthesis
+	* and a 0 is returned if they are equal
+	* note: alphabeticalness is decided based on ASCII so capital letters come first
+	* error:
+	* String s = "Apple";
+	* int a = s.compareTo"Aptitude";
+	* this would reuturn a */
+    }
+}

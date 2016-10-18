9b3c02e7a50502266ac75555d5945cbcbc2a8ee2
fspadea <fs279@pvsd.org>
2016-10-18 14:23:20 -0400

    wsdf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ public class Main {
 	/* public int compareTo(object myObj)
 	* can take any object but we will specifically be talking about it accepting Strings
 	* takes a String and alphabetically compares it to another String to see which String would come first in an alphabetically ordered list.
-	* It returns a positive integer if the String making the call precedes the String in the paranthesis
-	* a negative is returned if the String comes after the one in the paranthesis
+	* It returns a negative integer if the String making the call precedes the String in the paranthesis
+	* a positive is returned if the String comes after the one in the paranthesis
 	* and a 0 is returned if they are equal
 	* note: alphabeticalness is decided based on ASCII so capital letters come first
 	* error:
@@ -16,7 +16,7 @@ public class Main {
 	* this would return a positive number since the second p in "Apple" comes before the t in "Aptitude"*/
         String s = "pie";
         int x = s.compareTo("pipe");
-        if(x>0)
-            System.out.print("pie is first");
+        if(x<0)
+        System.out.print(x);
     }
 }
